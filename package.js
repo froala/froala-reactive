10d290d05e5cf3fdc8efa74be89789ac117efba9
Richard Smith <rsmith@rsbatechnology.co.uk>
2015-10-22 22:24:38 +0100

    Bump package rc version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'froala:editor-reactive',
   summary: 'A Meteor reactive template wrapper around Froala WYSIWYG HTML Rich Text Editor.',
-  version: '2.0.0-rc.2',
+  version: '2.0.0-rc.2-1',
   git: 'https://github.com/froala/froala-reactive.git'
 });
 

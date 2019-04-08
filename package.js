905ef6e4096196e89b54311eb3cffbdd9c7c5cb4
Navneet Kumar <navneet.kumar@accoliteindia.com>
2019-04-08 18:08:15 +0530

    Updated to 2.9.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'froala:editor-reactive',
   summary: 'A Meteor reactive template wrapper around Froala WYSIWYG HTML Rich Text Editor.',
-  version: '2.9.3',
+  version: '2.9.4',
   git: 'https://github.com/froala/froala-reactive.git'
 });
 
